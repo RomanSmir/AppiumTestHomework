54675f5f8165528c09d40aebf5584de6fde4205b
RomanSmir <romasmirnovmail@gmail.com>
2018-11-15 17:40:54 +0300

    Проверка по тексту

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,10 +3,15 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
-import  java.io.File;
+
 
 
 public class MainTest {
@@ -38,9 +43,52 @@ public class MainTest {
         driver.quit();
     }
 
-    @Test
+
+
+@Test
     public void firstTest()
-    {
-        System.out.println("First Test run");
-    }
+{
+   WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+    element_to_init_search.click();
+
+    WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+            "//*[contains(@text, 'Search…')]",
+            "Cannot find search input"
+    );
+
+    element_to_enter_search_line.sendKeys("Java");
+
+    waitForElementPresentByXpath(
+         "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Java (programming language)']",
+         "Cannot find 'Java (programming language)'Java'",
+         20
+         );
+
+  // WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+  //         "//*[contains(@text,'Search…')]",
+  //         "Cannot find search input,"
+  // );
+
+
+
+}
+
+private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds)
+
+{
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(error_message+"\n");
+    By by = By.xpath(xpath);
+    return wait.until(
+            ExpectedConditions.presenceOfElementLocated(by)
+            
+    );
+
+}
+
+private WebElement waitForElementPresentByXpath(String xpath, String error_message)
+{
+    return waitForElementPresentByXpath(xpath,error_message,5);
+}
+
 }
